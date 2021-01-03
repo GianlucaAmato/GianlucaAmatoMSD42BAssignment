020500e7c682595cc26f1baa8afa0f905d70fcfd
Gla0262 <gianluca.amato.e21303@mcast.edu.mt>
2021-01-03 14:06:24 +0100

    Background Scrolling Added

---
+++ HomeAssignment/CarGame/Assets/Scripts/BGScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    [SerializeField] float BGSpeed = 0.05f;
+
+    Material Road;
+
+    Vector2 offset;
+    void Start()
+    {
+        Road = GetComponent<Renderer>().material;
+
+        offset = new Vector2(0f, BGSpeed);
+    }
+
+    void Update()
+    {
+        Road.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
